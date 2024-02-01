4b5fb0b9d78e9cd5a69d3d16064fd354092a3c78
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-01 20:08:19 +0200

    Finish Start Screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,29 @@ changeTheme.addEventListener("click", () => {
   window.localStorage.setItem("theme", isDarkTheme ? "moon" : "sun");
   changeTheme.classList.toggle("bx-sun", !isDarkTheme);
 });
+
+// Difficult Game
+const form = document.querySelector("form#diff");
+const startBtn = document.querySelector(".start-btn");
+
+const savedValue = localStorage.getItem("difficulty");
+if (savedValue) {
+  const radio = document.querySelector(
+    `input[name='diff'][value='${savedValue}']`
+  );
+  if (radio) {
+    radio.checked = true;
+  }
+}
+
+form.onsubmit = function (e) {
+  e.preventDefault();
+  const selectedRadio = document.querySelector(
+    "form#diff input[name='diff']:checked"
+  );
+  const selectedValue = selectedRadio ? selectedRadio.value : null;
+
+  if (selectedValue) {
+    localStorage.setItem("difficulty", selectedValue);
+  }
+};
